b9dd0d843dd1e73ac889bdac46f8f98fc0eb99ee
Astrolover999 <150594683+Astrolover999@users.noreply.github.com>
2023-11-12 19:29:17 +0700

    2.py
    Изменила ошибку

--- 2.py
+++ 2.py
@@ -1,11 +1,11 @@
-list1 = [int(i) for i in input().split()]
-if len(list1) == 1:
-    print(list1[0])
-elif len(list1) == 2:
-    print(list1[1]*2, list1[0]*2)
+a= [int(i) for i in input().split()]
+if len(a) == 1:
+    print(a[0])
+elif len(a) == 2:
+    print(a[1]*2, a[0]*2)
 else:
-    for i in range(len(list1)):
-        if i < len(list1) - 1:
-            print(list1[i-1] + list1[i+1], end=" " )
+    for i in range(len(a)):
+        if i < len(a) - 1:
+            print(a[i-1] + a[i+1], end=" " )
         else:
-            print(list1[0] + list1[i-1])
+            print(a[0] + a[i-1])
